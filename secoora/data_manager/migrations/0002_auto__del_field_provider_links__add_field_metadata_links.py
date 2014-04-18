a05adb75462986c1ccf5675a31cfde1bbce2da02
jcothran <jeremy.cothran@gmail.com>
2014-04-18 07:00:43 -0700

    Migrations

---
+++ secoora/data_manager/migrations/0002_auto__del_field_provider_links__add_field_metadata_links.py
@@ -0,0 +1,194 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Deleting field 'Provider.links'
+        db.delete_column('data_manager_provider', 'links')
+
+        # Adding field 'Metadata.links'
+        db.add_column('data_manager_metadata', 'links',
+                      self.gf('django.db.models.fields.TextField')(default='', blank=True),
+                      keep_default=False)
+
+    def backwards(self, orm):
+        # Adding field 'Provider.links'
+        db.add_column('data_manager_provider', 'links',
+                      self.gf('django.db.models.fields.TextField')(default='', blank=True),
+                      keep_default=False)
+
+        # Deleting field 'Metadata.links'
+        db.delete_column('data_manager_metadata', 'links')
+
+    models = {
+        'data_manager.attributeinfo': {
+            'Meta': {'object_name': 'AttributeInfo'},
+            'display_name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'field_name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'order': ('django.db.models.fields.IntegerField', [], {'default': '1'}),
+            'precision': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'})
+        },
+        'data_manager.funding': {
+            'Meta': {'object_name': 'Funding'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.TextField', [], {})
+        },
+        'data_manager.layer': {
+            'Meta': {'object_name': 'Layer'},
+            'arcgis_layers': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'attribute_event': ('django.db.models.fields.CharField', [], {'default': "'click'", 'max_length': '35'}),
+            'attribute_fields': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': "orm['data_manager.AttributeInfo']", 'null': 'True', 'blank': 'True'}),
+            'attribute_title': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'bookmark': ('django.db.models.fields.CharField', [], {'max_length': '755', 'null': 'True', 'blank': 'True'}),
+            'compress_display': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'data_download': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'data_notes': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'data_overview': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'data_source': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'data_status': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'fact_sheet': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_sublayer': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'kml': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'layer_type': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'learn_more': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'legend': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'legend_subtitle': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'legend_title': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'lookup_field': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'lookup_table': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': "orm['data_manager.LookupInfo']", 'null': 'True', 'blank': 'True'}),
+            'map_tiles': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'metadata': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'metadatatable': ('django.db.models.fields.related.OneToOneField', [], {'blank': 'True', 'related_name': "'layer'", 'unique': 'True', 'null': 'True', 'to': "orm['data_manager.Metadata']"}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'opacity': ('django.db.models.fields.FloatField', [], {'default': '0.5', 'null': 'True', 'blank': 'True'}),
+            'openlayers_options': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'provider': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': "orm['data_manager.Provider']", 'null': 'True', 'blank': 'True'}),
+            'row_entry_date': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'row_update_date': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'shareable_url': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'slug_name': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'source': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'sublayers': ('django.db.models.fields.related.ManyToManyField', [], {'blank': 'True', 'related_name': "'sublayers_rel_+'", 'null': 'True', 'to': "orm['data_manager.Layer']"}),
+            'themes': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': "orm['data_manager.Theme']", 'null': 'True', 'blank': 'True'}),
+            'thumbnail': ('django.db.models.fields.URLField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'topics': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': "orm['data_manager.Topic']", 'null': 'True', 'blank': 'True'}),
+            'url': ('django.db.models.fields.CharField', [], {'max_length': '2000', 'null': 'True', 'blank': 'True'}),
+            'utfurl': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'vector_color': ('django.db.models.fields.CharField', [], {'max_length': '7', 'null': 'True', 'blank': 'True'}),
+            'vector_fill': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
+            'vector_graphic': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'})
+        },
+        'data_manager.lookupinfo': {
+            'Meta': {'object_name': 'LookupInfo'},
+            'color': ('django.db.models.fields.CharField', [], {'max_length': '7', 'null': 'True', 'blank': 'True'}),
+            'dashstyle': ('django.db.models.fields.CharField', [], {'default': "'solid'", 'max_length': '11'}),
+            'fill': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'graphic': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'value': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'})
+        },
+        'data_manager.metadata': {
+            'Meta': {'object_name': 'Metadata'},
+            'abstract': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'anytext': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'bbox_extent': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'data_type': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'description_short': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'getcap_link': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'identifiers': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'keywords_funding': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': "orm['data_manager.Funding']", 'null': 'True', 'blank': 'True'}),
+            'keywords_instrumentation': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'keywords_obs': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': "orm['data_manager.Observation']", 'null': 'True', 'blank': 'True'}),
+            'keywords_other': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'keywords_place': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': "orm['data_manager.Place']", 'null': 'True', 'blank': 'True'}),
+            'keywords_project': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': "orm['data_manager.ProjectName']", 'null': 'True', 'blank': 'True'}),
+            'keywords_theme': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': "orm['data_manager.Theme']", 'null': 'True', 'blank': 'True'}),
+            'keywords_topic': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': "orm['data_manager.Topic']", 'null': 'True', 'blank': 'True'}),
+            'links': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'metadata_link': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'model_data_type': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'publish_date': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'spatial_resolution': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'spatial_type': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'thumbnail_product': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'time_begin': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'time_end': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'time_interval_minutes': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'time_type': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'title': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'wkt_geometry': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'xml': ('django.db.models.fields.TextField', [], {'blank': 'True'})
+        },
+        'data_manager.observation': {
+            'Meta': {'object_name': 'Observation'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.TextField', [], {})
+        },
+        'data_manager.place': {
+            'Meta': {'object_name': 'Place'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.TextField', [], {})
+        },
+        'data_manager.projectname': {
+            'Meta': {'object_name': 'ProjectName'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.TextField', [], {})
+        },
+        'data_manager.provider': {
+            'Meta': {'object_name': 'Provider'},
+            'catalog_link': ('django.db.models.fields.CharField', [], {'max_length': '500', 'blank': 'True'}),
+            'catalog_name': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'contact_email_list': ('django.db.models.fields.CharField', [], {'max_length': '2000', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'metadatatable': ('django.db.models.fields.related.OneToOneField', [], {'blank': 'True', 'related_name': "'provider'", 'unique': 'True', 'null': 'True', 'to': "orm['data_manager.Metadata']"}),
+            'row_entry_date': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'row_update_date': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'source_link': ('django.db.models.fields.CharField', [], {'max_length': '500', 'blank': 'True'}),
+            'source_name': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'thumbnail_source': ('django.db.models.fields.CharField', [], {'max_length': '500', 'blank': 'True'}),
+            'use_constraints': ('django.db.models.fields.CharField', [], {'max_length': '2000', 'blank': 'True'})
+        },
+        'data_manager.theme': {
+            'Meta': {'object_name': 'Theme'},
+            'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'display_name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'factsheet_link': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'factsheet_thumb': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'feature_excerpt': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'feature_image': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'feature_link': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'header_attrib': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'header_image': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'overview': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'thumbnail': ('django.db.models.fields.URLField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'})
+        },
+        'data_manager.topic': {
+            'Meta': {'object_name': 'Topic'},
+            'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'display_name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'factsheet_link': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'factsheet_thumb': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'feature_excerpt': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'feature_image': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'feature_link': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'header_attrib': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'header_image': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'overview': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'thumbnail': ('django.db.models.fields.URLField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['data_manager']
