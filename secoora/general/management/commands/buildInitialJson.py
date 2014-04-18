6b4a176b4fb3b5cfe282d7e42fbbb4544c911511
dramage <danramage@Dans-MacBook-Pro.local>
2014-04-18 13:25:08 -0400

    Initial check in

---
+++ secoora/general/management/commands/buildInitialJson.py
@@ -0,0 +1,59 @@
+
+import sys
+import optparse
+import simplejson as json
+import traceback
+from data_manager.models import *
+
+def buildObsEntries(filename, modelData):
+  try:
+    obsTypeFile = open(filename, 'rU')
+  except IOError,e:
+    traceback.print_exc(e)
+  else:
+    pk = 1
+    for obs in obsTypeFile:
+      name = obs.strip()
+      modelData.append(
+        {
+          "pk": pk,
+          "model": "data_manager.observation",
+          "fields": {
+          'name' : name,
+          'display_name' : name.replace('_', ' ').title()
+          }
+        }
+      )
+      pk += 1
+    obsTypeFile.close()
+
+def buildKeywordsAny():
+  print "Building Keywords Any\n"
+  for layer in Layer.objects.all().order_by('name'):
+    print "Layer: name\n"
+
+def main():
+  parser = optparse.OptionParser()
+  parser.add_option("-o", "--ObsTypeFile", dest="obsTypeFile",
+                    help="" )
+  parser.add_option("-i", "--InitialJSONFile", dest="initialJsonFile",
+                    help="" )
+  parser.add_option('b', '--BuildKeywordsAny', dest="buildKeywordsAny")
+  (options, args) = parser.parse_args()
+
+  modelData = []
+  if(options.obsTypeFile):
+    buildObsEntries(options.obsTypeFile, modelData)
+  if(options.buildKeywordsAny):
+    buildKeywordsAny()
+
+  try:
+    outFile = open(options.initialJsonFile, 'w')
+  except IOError,e:
+    traceback.print_exc(e)
+  else:
+    outFile.write(json.dumps(modelData, sort_keys=True, indent=4))
+    outFile.close()
+
+if __name__ == '__main__':
+  main()
