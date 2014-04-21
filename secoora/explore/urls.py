4f03ceacaa516f84c9d4f3d8f81d152c3c267f12
dramage <danramage@Dans-MacBook-Pro.local>
2014-04-21 08:07:03 -0400

    Reworking catalog search.

--- secoora/explore/urls.py
+++ secoora/explore/urls.py
@@ -3,7 +3,7 @@ from views import *
 
 urlpatterns = patterns('',
     (r'^catalog/(?P<catalog_q>)$', 'catalog_search'),
-    (r'^catalog', data_catalog),
+    #(r'^catalog', data_catalog),
     #(r'^needs', data_needs),
     #(r'^map_tile_example/([\w-]*)', map_tile_example),
     #(r'^map_tile_esri_example/([\w-]*)', map_tile_esri_example),
