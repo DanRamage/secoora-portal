e0d1e0e8144706ce0ce779f08fed905c6242b653
dramage <danramage@Dans-MacBook-Pro.local>
2014-05-05 11:00:06 -0400

    Add function get_closest_time for finding the closest time for a given layer and time input.

--- secoora/data_manager/urls.py
+++ secoora/data_manager/urls.py
@@ -4,6 +4,6 @@ from views import *
 urlpatterns = patterns('',
     (r'^layer/([A-Za-z0-9_-]+)$', update_layer),
     (r'^layer', create_layer),
-    (r'^get_json', get_json)
+    (r'^get_json', get_json),
     (r'^get_closest_time', get_closest_time)
 )
