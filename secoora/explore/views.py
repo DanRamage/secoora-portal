80e79a601d5d7201e443fb2e5bec5a0915e42363
dramage <danramage@Dans-MacBook-Pro.local>
2014-05-07 13:13:29 -0400

    Testing bootstrap 3.

--- secoora/explore/views.py
+++ secoora/explore/views.py
@@ -19,6 +19,13 @@ def data_catalog(request, template='catalog.html'):
     context = {'themes': themes_with_links, 'domain': get_domain(8000), 'domain8010': get_domain()}
     return render_to_response(template, RequestContext(request, context)) 
 
+def data_catalog(request, template='bs3_catalog.html'):
+    themes = Theme.objects.all().order_by('display_name')
+    themes_with_links = add_learn_links(themes)
+    add_ordered_layers_lists(themes_with_links)
+    context = {'themes': themes_with_links, 'domain': get_domain(8000), 'domain8010': get_domain()}
+    return render_to_response(template, RequestContext(request, context))
+
 def data_needs(request, template='needs.html'):
     themes = Theme.objects.all().order_by('display_name')
     ordered_themes, theme_dict = add_ordered_needs_lists(themes)
