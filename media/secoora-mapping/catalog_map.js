c35f62fae5548b02c3c21ff9c4d432bfd5195b72
DanRamage <dan@inlet.geol.sc.edu>
2014-08-22 12:33:15 -0400

    ADding map for geographic catalog search.

--- media/secoora-mapping/catalog_map.js
+++ media/secoora-mapping/catalog_map.js
@@ -7,8 +7,7 @@ function catalog_search_map()
   self.polygonOLControl = null;
 
 
-  self.initialize = function(map_element_id)
-  {
+  self.initialize = function(map_element_id) {
     self.map = new OpenLayers.Map(map_element_id, {
       displayProjection: new OpenLayers.Projection("EPSG:4326"),
       projection: "EPSG:102113"
@@ -21,39 +20,36 @@ function catalog_search_map()
       attribution: "Sources: GEBCO, NOAA, CHS, OSU, UNH, CSUMB, National Geographic, DeLorme, NAVTEQ, and Esri"
     });
 
-  }
-  //Add a vector layer to draw our polygon selection on for use with the "Does layer have data here" ESRI quesry.
-  var polygonLayer = new OpenLayers.Layer.Vector("Polygon Layer", {
-                                                  styleMap : new OpenLayers.StyleMap({'default' : pgStyle})
+    //Add a vector layer to draw our polygon selection on for use with the "Does layer have data here" ESRI quesry.
+    var polygonLayer = new OpenLayers.Layer.Vector("Polygon Layer", {
+      styleMap: new OpenLayers.StyleMap({'default': pgStyle})
     });
 
-  self.map.addLayers(esriOcean, polygonLayer);
+    self.map.addLayers(esriOcean, polygonLayer);
 
 
-  self.polygonOLControl = new OpenLayers.Control.DrawFeature(polygonLayer,
-                                                        OpenLayers.Handler.Polygon,
-                                                        {
-                                                          featureAdded : self.selectionPolygonAdded,
-                                                          callbacks : {
-                                                            //We want to check to see if we're drawing a new polygon. If we are, let's get
-                                                            //rid of the previous one from the screen.
-                                                            point : function(point)
-                                                            {
-                                                              //We have a polygon on the screen already, we're getting reading to create a new
-                                                              //one, so clear the old one.
-                                                              if(self.polygonQueryGeom && self.polygonQueryGeom.length)
-                                                              {
-                                                                //Remove the current polygon from the map.
-                                                                this.layer.removeAllFeatures();
-                                                                //Reset the polygon points array.
-                                                                self.polygonQueryGeom.length = 0;
-                                                              }
-                                                            }
+    self.polygonOLControl = new OpenLayers.Control.DrawFeature(polygonLayer,
+      OpenLayers.Handler.Polygon,
+      {
+        featureAdded: self.selectionPolygonAdded,
+        callbacks: {
+          //We want to check to see if we're drawing a new polygon. If we are, let's get
+          //rid of the previous one from the screen.
+          point: function (point) {
+            //We have a polygon on the screen already, we're getting reading to create a new
+            //one, so clear the old one.
+            if (self.polygonQueryGeom && self.polygonQueryGeom.length) {
+              //Remove the current polygon from the map.
+              this.layer.removeAllFeatures();
+              //Reset the polygon points array.
+              self.polygonQueryGeom.length = 0;
+            }
+          }
 
-                                                          }
+        }
 
-  });
-  self.map.addControl(self.polygonOLControl);
-  self.polygonOLControl.enableControl(true);
-
-}
+      });
+    self.map.addControl(self.polygonOLControl);
+    self.polygonOLControl.enableControl(true);
+  };
+};
