3a77c6cc2a160645162dee92592ee5278d6ecf46
DanRamage <dan@inlet.geol.sc.edu>
2014-08-22 11:24:19 -0400

    Fixing layer count in catalog.

---
+++ media/secoora-mapping/catalog_map.js
@@ -0,0 +1,59 @@
+
+
+function catalog_search_map()
+{
+  var self = this;
+  self.map = null;
+  self.polygonOLControl = null;
+
+
+  self.initialize(map_element_id)
+  {
+    self.map = new OpenLayers.Map(map_element_id, {
+      displayProjection: new OpenLayers.Projection("EPSG:4326"),
+      projection: "EPSG:102113"
+    });
+
+    esriOcean = new OpenLayers.Layer.XYZ("ESRI Ocean", "http://services.arcgisonline.com/ArcGIS/rest/services/Ocean_Basemap/MapServer/tile/${z}/${y}/${x}", {
+      sphericalMercator: true,
+      isBaseLayer: true,
+      //numZoomLevels: 13,
+      attribution: "Sources: GEBCO, NOAA, CHS, OSU, UNH, CSUMB, National Geographic, DeLorme, NAVTEQ, and Esri"
+    });
+
+  }
+  //Add a vector layer to draw our polygon selection on for use with the "Does layer have data here" ESRI quesry.
+  var polygonLayer = new OpenLayers.Layer.Vector("Polygon Layer", {
+                                                  styleMap : new OpenLayers.StyleMap({'default' : pgStyle})
+    });
+
+  self.map.addLayers(esriOcean, polygonLayer);
+
+
+  self.polygonOLControl = new OpenLayers.Control.DrawFeature(polygonLayer,
+                                                        OpenLayers.Handler.Polygon,
+                                                        {
+                                                          featureAdded : self.selectionPolygonAdded,
+                                                          callbacks : {
+                                                            //We want to check to see if we're drawing a new polygon. If we are, let's get
+                                                            //rid of the previous one from the screen.
+                                                            point : function(point)
+                                                            {
+                                                              //We have a polygon on the screen already, we're getting reading to create a new
+                                                              //one, so clear the old one.
+                                                              if(self.polygonQueryGeom && self.polygonQueryGeom.length)
+                                                              {
+                                                                //Remove the current polygon from the map.
+                                                                this.layer.removeAllFeatures();
+                                                                //Reset the polygon points array.
+                                                                self.polygonQueryGeom.length = 0;
+                                                              }
+                                                            }
+
+                                                          }
+
+  });
+  self.map.addControl(self.polygonOLControl);
+  self.polygonOLControl.enableControl(true);
+
+}
