272b8516f16da0b23a3b56fbb69134003400c025
DanRamage <dan@inlet.geol.sc.edu>
2014-09-17 14:19:20 -0400

    Added links_data property for pycsw model.

--- secoora/data_manager/models.py
+++ secoora/data_manager/models.py
@@ -1001,7 +1001,7 @@ class pycsw_records(models.Model):
       buf = datetime.datetime.strptime(self.time_start, '%Y-%m-%dT%H:%M:%SZ')
       buf = buf.strftime("%Y-%m-%d %H:%M:%S")
       if logger:
-        logger.debug("time_begin_pretty: %s" % (buf.strftime("%Y-%m-%d %H:%M:%S")))
+        logger.debug("time_begin_pretty: %s" % (buf))
     except Exception,e:
       buf = self.time_start
       if logger:
@@ -1014,7 +1014,7 @@ class pycsw_records(models.Model):
       buf = datetime.datetime.strptime(self.time_end, '%Y-%m-%dT%H:%M:%SZ')
       buf = buf.strftime("%Y-%m-%d %H:%M:%S")
       if logger:
-        logger.debug("time_end_pretty: %s" % (buf.strftime("%Y-%m-%d %H:%M:%S")))
+        logger.debug("time_end_pretty: %s" % (buf))
     except Exception,e:
       buf = self.time_end
       if logger:
