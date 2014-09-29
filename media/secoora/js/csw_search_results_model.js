d6bb0215abf62381a987cd52e6f9d2b310e12708
DanRamage <dan@inlet.geol.sc.edu>
2014-09-29 11:22:48 -0400

    Testing search results page.

--- media/secoora/js/csw_search_results_model.js
+++ media/secoora/js/csw_search_results_model.js
@@ -5,7 +5,24 @@ function csw_search_model() {
   self.resultsCount = ko.observable(0);
 
 
-  self.process_results = function () {
+  self.initialize = function(csw_results) {
+    if('tag' in csw_results)
+    {
+      //Verify the result is a GetRecordsResponse.
+      if(csw_results['tag'] == 'csw:GetRecordsResponse')
+      {
+        //pyCSW returns results with 2 elements in the children array.
+        //The first is csw:SearchStatus which seems to only contain a time,
+        //and then the csw:SearchResults which is what we want.
+        $.each(csw_results.children, function(ndx, child)
+        {
+          if(child['tag'] == 'csw:SearchResults')
+          {
+            self.resultsCount(child['attributes'].numerOfRecordsReturned);
+            self.results(child.children);
+          }
+        });
+      }
+    }
   }
-
 }
