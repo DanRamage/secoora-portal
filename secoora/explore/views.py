0b5d71d20cf546066b0250991079e93614abea9c
DanRamage <dan@inlet.geol.sc.edu>
2014-09-29 15:34:59 -0400

    Testing search results page.

--- secoora/explore/views.py
+++ secoora/explore/views.py
@@ -218,7 +218,7 @@ def csw_query(request, template='csw_search_results.html'):
   if logger:
     logger.debug("catalog_search_results end")
   #return render_to_response(template, {'brand_info': simplejson.dumps(brand_json)}, context_instance=RequestContext(request))
-  return render_to_response(template, {'search_results': csw_records}, context_instance=RequestContext(request))
+  return render_to_response(template, {'search_results': results.text}, context_instance=RequestContext(request))
 
 
 
