92e9e86394f33e6671f90b115f13ff214127dabf
DanRamage <dan@inlet.geol.sc.edu>
2014-09-29 15:07:40 -0400

    Testing search results page.

--- secoora/explore/views.py
+++ secoora/explore/views.py
@@ -203,7 +203,12 @@ def csw_query(request, template='csw_search_results.html'):
     if(results.status_code == 200):
       if logger:
         logger.debug("Rcvd CSW results: %s" % (results.text))
-      csw_records = simplejson.loads(results.text);
+      try:
+        csw_records = simplejson.loads(results.text);
+      except simplejson.JSONDecodeError, e:
+        csw_records = [];
+        if logger:
+          logger.exception(e)
     else:
       csw_records = [];
       if logger:
