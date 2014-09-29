12675c99ad665e4978e76dab98f1318d93426e79
DanRamage <dan@inlet.geol.sc.edu>
2014-09-29 10:21:48 -0400

    Adding view handler for CSW query.

---
+++ media/secoora/js/csw_search_results_model.js
@@ -0,0 +1,11 @@
+function csw_search_model() {
+  var self = this;
+
+  self.results = ko.observableArray([]);
+  self.resultsCount = ko.observable(0);
+
+
+  self.process_results = function () {
+  }
+
+}
