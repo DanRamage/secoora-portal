c8f6e61bc6ae65aa661ef377b5d37fc347fb37ca
DanRamage <dan@inlet.geol.sc.edu>
2014-10-01 09:31:25 -0400

    Testing search results page.

--- media/secoora/js/csw_search_results_model.js
+++ media/secoora/js/csw_search_results_model.js
@@ -6,8 +6,7 @@ function csw_search_model() {
   self.resultsCount = ko.observable(0);
   self.errorMsg = ko.observable("");
 
-  self.process_results = function(data, textStatus, jqXHR) {
-    var search_results = data.responseText;
+  self.process_results = function(csw_results, textStatus, jqXHR) {
     if('tag' in csw_results)
     {
       //Verify the result is a GetRecordsResponse.
