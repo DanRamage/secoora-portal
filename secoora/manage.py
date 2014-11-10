f039984a6856337337cbd948ad28dff536dd2c18
DanRamage <dan@inlet.geol.sc.edu>
2014-11-10 10:23:30 -0500

    Add use_tls flag to enable secure connection for email.

--- secoora/manage.py
+++ secoora/manage.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 from django.core.management import execute_manager
+#import sys
+#from django.core.management import execute_from_command_line
 import imp
 try:
     imp.find_module('settings') # Assumed to be in the same directory.
