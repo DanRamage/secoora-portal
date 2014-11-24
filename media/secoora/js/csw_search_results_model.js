21d8becfa6b5240a84e0ac8045717c64c7684e36
DanRamage <dan@inlet.geol.sc.edu>
2014-11-24 14:24:25 -0500

    Change CSW query IP to static IP.

--- media/secoora/js/csw_search_results_model.js
+++ media/secoora/js/csw_search_results_model.js
@@ -30,7 +30,8 @@ function csw_search_model() {
 
   self.init_map = function (html_map_id) {
     self.mapView = catalog_search_map();
-    self.mapView.initialize(null, "/secoora_portal/proxy/rest_query/?url=http://129.252.139.68:8000", app.viewModel.process_results);
+    //self.mapView.initialize(null, "/secoora_portal/proxy/rest_query/?url=http://129.252.139.68:8000", app.viewModel.process_results);
+    self.mapView.initialize(null, "http://129.252.37.192:8000", app.viewModel.process_results);
     self.mapView.olMap.render('map');
     self.mapView.olMap.setCenter(new OpenLayers.LonLat(-73.852, 31.933).transform(
       new OpenLayers.Projection("EPSG:4326"),
