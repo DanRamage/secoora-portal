199e20947d85a10213dac4d8653f6021a7cf9b9a
DanRamage <dan@inlet.geol.sc.edu>
2014-12-05 12:32:03 -0500

    Added keywords collapsible

--- secoora/explore/views.py
+++ secoora/explore/views.py
@@ -235,6 +235,7 @@ def csw_list_service_type_grouping(request, template='pycsw_services_view.html')
             'html_id': type['protocol'].replace(':', '_').replace(' ', '_') + '_coverage',
             'records': []
           }
+        rec.keywords = rec.keywords.replace('_', ' ').split(',')
         spatial_type['coverage']['records'].append(rec)
 
       #service_types[type['protocol']]['records'].append(rec)
