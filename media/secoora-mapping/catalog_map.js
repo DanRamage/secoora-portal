6720414102c5f42995f62a51bb7b22de79259bdb
DanRamage <dan@inlet.geol.sc.edu>
2014-12-09 11:22:48 -0500

    WOrking on search page.

--- media/secoora-mapping/catalog_map.js
+++ media/secoora-mapping/catalog_map.js
@@ -15,7 +15,8 @@ function catalog_search_map()
   };
   self.keywordSearch = function(search_term)
   {
-    //var parse_results = searchQuery.parse(search_term);
+    var query_parser = new SearchQuery();
+    var parse_results = query_parser.parse(search_term);
     //var search_term = "";
     //Search filter for keyword as user typed.
     var user_term = new OpenLayers.Filter.Comparison({
