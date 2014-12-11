e5fdbea1c524e0928382e51e1e009361808d1553
DanRamage <dan@inlet.geol.sc.edu>
2014-12-11 10:57:37 -0500

    Added temporal search.

--- media/secoora/js/search_page_model.js
+++ media/secoora/js/search_page_model.js
@@ -42,7 +42,7 @@ function search_page_model() {
     self.mapView = catalog_search_map();
     //self.mapView.initialize(null, "/secoora_portal/proxy/rest_query/?url=http://129.252.139.68:8000", app.viewModel.process_results);
     //self.mapView.initialize(null, "/secoora_portal/proxy/rest_query/?url=http://129.252.37.192:8000", app.viewModel.process_results);
-    self.mapView.initialize(null, catalog_url, app.viewModel.process_results);
+    self.mapView.initialize(null, catalog_url, app.viewModel.process_results_xml);
 
     self.mapView.olMap.render(html_map_id);
 
@@ -109,6 +109,10 @@ function search_page_model() {
       return;
     }
     self.mapView.temporalSearch(start_string, end_string);
+  }
+  self.process_results_xml = function (csw_results, textStatus, jqXHR) {
+    self.results.removeAll();
+
   }
   self.process_results = function (csw_results, textStatus, jqXHR) {
     self.results.removeAll();
