bd34f332c790d9811082c1cbd7b99cc59b1fc372
DanRamage <dan@inlet.geol.sc.edu>
2015-04-28 07:58:30 -0400

    ADding rest handler for getting water temperatures.

--- secoora/data_manager/views.py
+++ secoora/data_manager/views.py
@@ -226,10 +226,10 @@ def get_water_temp_stations(request):
     if id is not None:
       bbox = "POLYGON((%s))" % (SECOORA_BBOX)
       platform_list = xeniaDb.session.query(xenia_platform.row_id, xenia_platform.platform_handle)\
-        .join((xenia_sensor, xenia_sensor.m_type_id == m_type_id))\
+        .join((xenia_sensor, xenia_sensor.m_type_id == xenia_platform.row_id))\
+        .filter(func.ST_Contains(WKTElement(bbox, srid=4326), WKBElement(xenia_platform.the_geom, srid=4326)))\
         .filter(xenia_sensor.m_type_id == m_type_id)\
         .filter(xenia_platform.active.in_((1,2)))\
-        .filter(func.ST_Contains(WKTElement(bbox, srid=4326), WKBElement(xenia_platform.the_geom, srid=4326)))\
         .order_by(xenia_platform.short_name)
     platforms = []
     for platform in platform_list:
