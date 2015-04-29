228c47a9855d78a4151868774c69effcc0e41c68
DanRamage <dan@inlet.geol.sc.edu>
2015-04-29 11:53:53 -0400

    Adding gradient style building.

--- media/secoora-mapping/map.js
+++ media/secoora-mapping/map.js
@@ -962,9 +962,9 @@ window.alert(start_date.toISOString()+'/'+end_date.toISOString());
             var rules = style_bldr.build_gradient('#0000ff', '#ff0000', layer.openlayers_options.steps, layer.openlayers_options.label_property);
             var style = new OpenLayers.Style(
                 {
-                    strokeWidth: '${strokeFunction}',
+                    //strokeWidth: '${strokeFunction}',
                     //strokeOpacity: 0.5,
-                    pointRadius: '${radiusfunction}',
+                    //pointRadius: '${radiusfunction}',
                     label: "${" + layer.openlayers_options.label_property + "}",
                     fontColor: "#000000"
                 },
