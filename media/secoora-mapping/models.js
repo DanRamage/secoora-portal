f78e2903c391d8c268adbe821492a31eb554427a
DanRamage <dan@inlet.geol.sc.edu>
2015-05-06 12:34:04 -0400

    Adding plot code.

--- media/secoora-mapping/models.js
+++ media/secoora-mapping/models.js
@@ -2472,7 +2472,7 @@ function viewModel() {
       var view_px = app.map.getViewPortPxFromLonLat(lon_lat);
       $('#obs-click-popup').show().offset({top: map_offset.top + view_px.y + 5, left: map_offset.left + view_px.x + 5});
 
-      $.plot($("#obs-click-popup #plot_area"), flot_data,
+      $.plot($("#obs-click-popup #plot_area"), [flot_data],
       {
         xaxis: {mode: "time"},
         crosshair: {mode: 'x'   },
