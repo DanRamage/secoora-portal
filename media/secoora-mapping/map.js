816e5e14576a676e34237ec5837e8b093eeb1851
DanRamage <dan@inlet.geol.sc.edu>
2015-05-08 13:25:06 -0400

    ADding wind geojson layer.

--- media/secoora-mapping/map.js
+++ media/secoora-mapping/map.js
@@ -1121,7 +1121,11 @@ app.addLayerToMap = function(layer, isVisible) {
               projection: "EPSG:4326",
               protocol: new OpenLayers.Protocol.HTTP({
                   url: layer.openlayers_options.geojson,
-                  format: new OpenLayers.Format.GeoJSON()
+                  format: new OpenLayers.Format.GeoJSON(),
+                  callback: function()
+                  {
+                    var i = 0;
+                  }
               }),
               strategies: strategies,
               styleMap: style_map
