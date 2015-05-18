bf9b982781e438524c2ef8a6c1d5bd06d1f87587
DanRamage <dan@inlet.geol.sc.edu>
2015-05-18 15:02:41 -0400

    Add in the HF radar remote sensing layers.

--- secoora/general/management/commands/updateTimeSteps.sh
+++ secoora/general/management/commands/updateTimeSteps.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-/usr/bin/python /usr/local/userapps/secoora-portal/secoora/manage.py updateMetadata --UpdateTimeSteps --RemoteSensingLayers="Interpolated Sea Surface Temperature,MODIS Chlorophyll,MODIS Cloud Cover,Sea Surface Temperature(MODIS)"> /tmp/UpdateTimeSteps.log 2>&1
+/usr/bin/python /usr/local/userapps/secoora-portal/secoora/manage.py updateMetadata --UpdateTimeSteps --RemoteSensingLayers="HF Radar - Miami (UM),HF Radar - Outer Banks (UNC),Interpolated Sea Surface Temperature,MODIS Chlorophyll,MODIS Cloud Cover,Sea Surface Temperature(MODIS)"> /tmp/UpdateTimeSteps.log 2>&1
