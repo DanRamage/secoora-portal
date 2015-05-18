cc2c9634b1e32feda5300faf7ffe9d5d5804e0a3
DanRamage <dan@inlet.geol.sc.edu>
2015-05-18 12:10:39 -0400

    ADd check for qc_level in data. If present, only allow good data out.

--- secoora/data_manager/views.py
+++ secoora/data_manager/views.py
@@ -322,7 +322,7 @@ def get_obs_data(obs_name, uom_name):
                 #If we have qc data, we want to check it and only allow good data out.
                 qc_level = prop['qc_level']
                 allow_rec = True
-                if len(qc_level[-1]) and (qc_level[-1] != ''):
+                if len(qc_level) and (qc_level[-1] != ''):
                   if(int(qc_level[-1]) < 3):
                     if logger:
                       logger.debug("Platform: %s obs: %s has failed qc: %s" % (properties['p_handle'], obs_name, qc_level[-1]))
