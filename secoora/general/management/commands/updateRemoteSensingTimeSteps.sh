c947709fe5c839ccc81133f0412b62c5ae12c4d1
DanRamage <dan@inlet.geol.sc.edu>
2015-05-19 10:43:12 -0400

    ADded outer banks hf radar to remote sensing time pull.

--- secoora/general/management/commands/updateRemoteSensingTimeSteps.sh
+++ secoora/general/management/commands/updateRemoteSensingTimeSteps.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-/usr/bin/python /usr/local/userapps/secoora-portal/secoora/manage.py updateMetadata --RemoteSensingLayers="HF Radar - Miami (UM),Interpolated Sea Surface Temperature,MODIS Chlorophyll,MODIS Cloud Cover,Sea Surface Temperature(MODIS)"> /tmp/UpdateTimeSteps.log 2>&1
+/usr/bin/python /usr/local/userapps/secoora-portal/secoora/manage.py updateMetadata --RemoteSensingLayers="HF Radar - Outer Banks (UNC),HF Radar - Miami (UM),Interpolated Sea Surface Temperature,MODIS Chlorophyll,MODIS Cloud Cover,Sea Surface Temperature(MODIS)"> /tmp/UpdateTimeSteps.log 2>&1
